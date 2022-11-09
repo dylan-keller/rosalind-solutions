13880ce261262e3c55afdd9c441a8dab9c56a90e
keller <dylan.b.keller@gmail.com>
2022-11-09 22:26:20 +0100

    finishing ordering around the repository

--- dna.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    dna = line.strip()
-    for i in 'ACGT':
-        print(dna.count(i),'', end='')
-    break
--- rna.py
+++
@@ -1,7 +0,0 @@
-print("input dna strand")
-strand = input().strip()
-
-for i in range (0, len(strand)) :
-    if (strand[i] == 'T') : strand = strand[:i] + 'U' + strand[i+1:]
-
-print(strand)
